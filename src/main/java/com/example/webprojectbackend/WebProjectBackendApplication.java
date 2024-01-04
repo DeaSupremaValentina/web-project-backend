2d2341303dfb36307a542a41e78b038fabda38fc
ValentinaRicioppo <rcpvnt03l66d086z@studenti.unical.it>
2024-01-04 12:03:49 +0100

    Initial commit

---
+++ src/main/java/com/example/webprojectbackend/WebProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.webprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webprojectbackend/WebProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
