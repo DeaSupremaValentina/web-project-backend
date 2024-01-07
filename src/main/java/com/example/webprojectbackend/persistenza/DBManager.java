b8bb9743c1266a255e9452f06f99e67e11316d36
nika <129058062+nicolearnieri@users.noreply.github.com>
2024-01-07 12:20:24 +0100

    Db manager (a cui vanno aggiunti gli ogetti DAO) Verifica se la connessione (nome) è corretta

--- src/main/java/com/example/webprojectbackend/persistenza/DBManager.java
+++ src/main/java/com/example/webprojectbackend/persistenza/DBManager.java
@@ -1,4 +1,36 @@
 package com.example.webprojectbackend.persistenza;
 
-public class DBManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager
+{
+    private static DBManager instance = null;
+    private DBManager(){}
+
+    public static DBManager getInstance()
+    {
+        if (instance == null)
+            {   instance = new DBManager(); }
+        return instance;
+    }
+
+    Connection connection = null;
+
+    public Connection getConnection()
+    {
+        if (connection == null)
+        {
+            try
+            {
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres 2", "postgres", "root");
+            }
+            catch (SQLException e)
+            { throw new RuntimeException(e); }
+        }
+        return connection;
+    }
+
+    //aggiunta Dao
 }
