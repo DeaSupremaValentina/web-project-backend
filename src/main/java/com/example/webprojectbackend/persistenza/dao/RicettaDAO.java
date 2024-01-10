d91ebc9ffa06b7f4cec1aebe7b453bbb544e2502
nika <129058062+nicolearnieri@users.noreply.github.com>
2024-01-10 12:51:24 +0100

    RicettaDao

--- src/main/java/com/example/webprojectbackend/persistenza/dao/RicettaDAO.java
+++ src/main/java/com/example/webprojectbackend/persistenza/dao/RicettaDAO.java
@@ -7,7 +7,15 @@ import java.util.List;
 public interface RicettaDAO {
 
     public List<Ricetta> findAll();
+
+    public List<Ricetta> findByNome(String nome);
+
+    public List<Ricetta> findByCategoria(String categoria);
+
+    public List<Ricetta> findByTag(String tag); //tag 1 or tag 2 indifferente
     public Ricetta findByPrimaryKey(int id);
+
+    public List<Ricetta> findByUsername(String username); //ricette create da un utente
     public void saveOrUpdate(Ricetta recipe);
     public void delete(Ricetta recipe);
 }
