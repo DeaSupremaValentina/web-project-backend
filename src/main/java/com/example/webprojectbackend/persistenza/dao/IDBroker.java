44c5e7642d1099a0ba4eeaf7088ff01cc7194881
nika <129058062+nicolearnieri@users.noreply.github.com>
2024-01-11 13:09:45 +0100

    IDBroker per i codici univoci autoincrementali delle tabelle

---
+++ src/main/java/com/example/webprojectbackend/persistenza/dao/IDBroker.java
@@ -0,0 +1,40 @@
+package com.example.webprojectbackend.persistenza.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+public class IDBroker {
+
+
+    public static int getRicetteId(Connection connection) {
+        //tabella per le ricette
+        String query = "SELECT nextval('ricette_seq') AS id";
+        return Math.toIntExact(getId(connection, query));
+    }
+
+    public static int getValutazioniId(Connection connection) {
+        //tabella per le valutazioni
+        String query = "SELECT nextval('valutazioni_seq') AS id";
+        return Math.toIntExact(getId(connection, query));
+    }
+
+    public static int getCommentiId(Connection connection) {
+        //tabella per i commenti
+        String query = "SELECT nextval('commenti_seq') AS id";
+        return Math.toIntExact(getId(connection, query));
+    }
+
+    private static Long getId(Connection connection, String query) {
+        Long id = null;
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet result = statement.executeQuery();
+            result.next();
+            id = result.getLong("id");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
+}
