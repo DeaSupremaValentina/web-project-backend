0753c369b231b6f7604c0a815482bfbf60aa4459
nika <129058062+nicolearnieri@users.noreply.github.com>
2024-01-11 12:07:10 +0100

    CommentiDAO finito

--- src/main/java/com/example/webprojectbackend/persistenza/dao/CommentoDAO.java
+++ src/main/java/com/example/webprojectbackend/persistenza/dao/CommentoDAO.java
@@ -8,8 +8,11 @@ public interface CommentoDAO {
     public List<Commento> findByUser(String username);
     public List<Commento> findByCodiceRicetta(int codiceRicetta);
 
-    public void saveOrUpdate(Commento commento);
+    public void save(Commento commento);
+    public void update(Commento commento);
     public void delete(Commento commento);
+    public void deleteByUser(String username);
+    public void deleteByRicetta(int codiceRicetta);
 
     //mi sembra inutile tbh
     //public List<Commento> findAll();
--- src/main/java/com/example/webprojectbackend/persistenza/dao/postgres/CommentiDAOPostgres.java
+++ src/main/java/com/example/webprojectbackend/persistenza/dao/postgres/CommentiDAOPostgres.java
@@ -43,16 +43,95 @@ public class CommentiDAOPostgres implements CommentoDAO {
 
     @Override
     public List<Commento> findByCodiceRicetta(int codiceRicetta) {
-        return null;
+        List <Commento> commenti = new ArrayList<>();
+        String query = "select * from commenti where ricetta = ?";
+        try {
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setInt(1, codiceRicetta);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Commento commento = new Commento();
+                commento.setCodiceRicetta(rs.getInt("ricetta"));
+                commento.setUsername(rs.getString("username"));
+                commento.setContenuto(rs.getString("contenuto"));
+                commento.setCodiceCommento(rs.getInt("codice"));
+                commenti.add(commento);
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return commenti;
     }
 
     @Override
-    public void saveOrUpdate(Commento commento) {
+    public void save(Commento commento) {
+        String query = "insert into commenti (ricetta, username, contenuto) values (?, ?, ?)";
+        try {
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setInt(1, commento.getCodiceRicetta());
+            st.setString(2, commento.getUsername());
+            st.setString(3, commento.getContenuto());
+            st.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
+    @Override
+    public void update(Commento commento) {
+        String query = "update commenti set contenuto = ? and username = ? where codice = ?";
+        try {
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setString(1, commento.getContenuto());
+            st.setString(2, commento.getUsername());
+            st.setInt(3, commento.getCodiceCommento());
+            st.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     @Override
     public void delete(Commento commento) {
+        String query = "delete from commenti where codice = ?";
+        try {
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setInt(1, commento.getCodiceCommento());
+            st.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
+    }
+
+    @Override
+    public void deleteByUser(String username) {
+        String query = "delete from commenti where username = ?";
+        try {
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setString(1, username);
+            st.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteByRicetta(int codiceRicetta) {
+        String query = "delete from commenti where ricetta = ?";
+        try {
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setInt(1, codiceRicetta);
+            st.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
