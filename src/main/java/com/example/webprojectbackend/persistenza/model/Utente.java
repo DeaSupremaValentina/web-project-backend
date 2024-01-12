9fdd797805a44a1ba693695c7239b77ea7b985f4
nika <129058062+nicolearnieri@users.noreply.github.com>
2024-01-12 23:46:46 +0100

    Valentina ti odio

--- src/main/java/com/example/webprojectbackend/persistenza/model/Utente.java
+++ src/main/java/com/example/webprojectbackend/persistenza/model/Utente.java
@@ -2,4 +2,32 @@ package com.example.webprojectbackend.persistenza.model;
 
 public class Utente {
     //se ne doveva occupare Firebase
+    private String userCode; //codice univoco dell'utente, lo dà Firebase
+    private String tipo; //admin o user
+
+    private String email, nome;
+    private String pathImmagine;
+    public Utente() {}
+
+    public Utente(String userCode, String tipo) {
+        this.userCode = userCode;
+        this.tipo = tipo;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
 }
