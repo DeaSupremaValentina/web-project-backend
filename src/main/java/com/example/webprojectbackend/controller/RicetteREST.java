81c4eb3ef511f899e7c79182c02a2f235a085e02
YanMark42 <iannellomarco@gmail.com>
2024-02-02 19:26:28 +0100

    url fixed

--- src/main/java/com/example/webprojectbackend/controller/RicetteREST.java
+++ src/main/java/com/example/webprojectbackend/controller/RicetteREST.java
@@ -3,6 +3,7 @@ package com.example.webprojectbackend.controller;
 import com.example.webprojectbackend.persistenza.DBManager;
 import com.example.webprojectbackend.persistenza.model.Ricetta;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,25 +13,25 @@ import java.util.List;
 @RestController
 public class RicetteREST {
     //per la singola ricetta, info trovate tramite id
-    @GetMapping("/ricette/{id}")
-    public Ricetta getRicettaByID(int id){
+    @GetMapping("/ricette_id/{id}")
+    public Ricetta getRicettaByID(@PathVariable int id){
         return DBManager.getInstance().getRicettaDAO().findByPrimaryKey(id);
     }
 
     //per le ricette trovate tramite nome
-    @GetMapping("/ricette/{nome}")
+    @GetMapping("/ricette_nome/{nome}")
     public List<Ricetta> getRicettaByNome(String nome){
         return DBManager.getInstance().getRicettaDAO().findByNome(nome);
     }
 
     //per le ricette trovate tramite categoria
-    @GetMapping("/ricette/{categoria}")
+    @GetMapping("/ricette_categoria/{categoria}")
     public List<Ricetta> getRicettaByCategoria(String categoria){
         return DBManager.getInstance().getRicettaDAO().findByCategoria(categoria);
     }
 
     //per le ricette trovate tramite tag
-    @GetMapping("/ricette/{tag}")
+    @GetMapping("/ricette_tag/{tag}")
     public List<Ricetta> getRicettaByTag(String tag){
         return DBManager.getInstance().getRicettaDAO().findByTag(tag);
     }
