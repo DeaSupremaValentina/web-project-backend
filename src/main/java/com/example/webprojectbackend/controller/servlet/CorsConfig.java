557b5c9f4f0cdeaf8fd1e5c592abade338aeb7eb
YanMark42 <iannellomarco@gmail.com>
2024-02-10 17:23:04 +0100

    cors fixed

--- src/main/java/com/example/webprojectbackend/controller/servlet/CorsConfig.java
+++ src/main/java/com/example/webprojectbackend/controller/servlet/CorsConfig.java
@@ -10,43 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        System.out.println("CORS partito");
-        registry.addMapping("/login") // Specifica il percorso a cui applicare le politiche CORS (Cross-Origin Resource Sharing)
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS") // Specifica i metodi HTTP consentiti
                 .allowedHeaders("*");
-        registry.addMapping("/tipoUtente") // Specifica il percorso a cui applicare le politiche CORS (Cross-Origin Resource Sharing)
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS") // Specifica i metodi HTTP consentiti
-                .allowedHeaders("*");
-        registry.addMapping("/logout")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS") // Specifica i metodi HTTP consentiti
-                .allowedHeaders("*");
-        registry.addMapping("/salvaRicetta")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS") // Specifica i metodi HTTP consentiti
-                .allowedHeaders("*");
-        registry.addMapping("/rimuoviRicetta")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS") // Specifica i metodi HTTP consentiti
-                .allowedHeaders("*");
-        registry.addMapping("/ricetteSalvate")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS") // Specifica i metodi HTTP consentiti
-                .allowedHeaders("*");
-        registry.addMapping("/salvaProposta")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS") // Specifica i metodi HTTP consentiti
-                .allowedHeaders("*");
-        registry.addMapping("/ricetteScritteDaUtente")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS") // Specifica i metodi HTTP consentiti
-                .allowedHeaders("*");
-        registry.addMapping("/tutteLeRicetteProposte")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS") // Specifica i metodi HTTP consentiti
-                .allowedHeaders("*");
-
-}
+    }
 }
