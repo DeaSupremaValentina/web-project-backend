6f6766b907a00107b224ce99e615190e524aa91c
ValentinaRicioppo <rcpvnt03l66d086z@studenti.unical.it>
2024-02-10 18:08:42 +0100

    aggiustato le rest pt2

--- src/main/java/com/example/webprojectbackend/controller/CommentiREST.java
+++ src/main/java/com/example/webprojectbackend/controller/CommentiREST.java
@@ -23,7 +23,7 @@ public class CommentiREST
     }
 
     @PostMapping("/salvaCommento")
-    public void saveCommento(@RequestBody String json, String comm) {
+    public void saveCommento(@RequestBody String json, String comm, Utente utente) {
         @SuppressWarnings("deprecation")
         JsonElement jsonElement = new JsonParser().parse(json);
         int codiceRicetta = jsonElement.getAsJsonObject().get("id").getAsInt();
@@ -31,7 +31,7 @@ public class CommentiREST
         Commento commento = new Commento();
         commento.setCodiceRicetta(codiceRicetta);
         commento.setContenuto(commentoFromjson);
-        commento.setUsername(Utente.getInstance().getNome());
+        commento.setUsername(utente.getNome());
         DBManager.getInstance().getCommentoDAO().save(commento);
     }
 
